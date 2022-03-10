b0a0d6c767af10cec7ef37aaf0aa0e2bb7355772
capriceebanks <93594922+capriceebanks@users.noreply.github.com>
2022-03-10 00:46:47 +0000

    Delete src directory

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export {default as SearchUsername} from './SearchUsername'
-export {default as Repositories} from './Repositories'
-export {default as RepoListing} from './RepoListing'
-export {default as UserListing} from './UserListing'
-export { default as NavBar } from './NavBar'
-
